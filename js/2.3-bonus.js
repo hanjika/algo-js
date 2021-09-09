7f87a5c92b7d94faee8af734b10bf4aefd9f4ec9
Hanna Masanjika <hanna.masanjika@gmail.com>
2021-09-09 11:08:46 +0200

    Added 2.3 bonus

--- js/2.3-bonus.js
+++ js/2.3-bonus.js
@@ -1,8 +1,5 @@
-let i = 1;
-
-while (i <= 100) {
+for (i = 1; i <= 100; i++) {
     if (i % 2 === 0) {
         console.log(i + "\n");
-    }
-    i++;
+    }      
 }
--- js/2.4.js
+++ js/2.4.js
@@ -1,7 +1,7 @@
 let i = 1;
 
 while (i <= 100) {
-    if (i % 2 != 0) {
+    if (i % 2 !== 0) {
         console.log(i * 3 + "\n");
     } else {
         console.log(i / 2 + "\n");
