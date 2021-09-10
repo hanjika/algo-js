8fe8261fd57bef1b1aaeca9313042b1912a7081c
Hanna Masanjika <hanna.masanjika@gmail.com>
2021-09-10 11:23:39 +0200

    Added 5.2

--- js/5/5.2.js
+++ js/5/5.2.js
@@ -4,6 +4,8 @@ let series = {
     cast: []
 };
 
+let result = [];
+
 function askTvSeries() {
     series.name = window.prompt("What is the name of your favourite TV series?");
     series.year = Number(window.prompt("In which year was this TV series produced?"));
@@ -11,4 +13,14 @@ function askTvSeries() {
     return series;
 }
 
-document.write(JSON.stringify(askTvSeries()));
+function randomizeCast(tvSeries) {
+    let n = tvSeries.cast.length;
+    for (let i = 0; i < n; i++) {
+        let randnum = Math.floor(Math.random() * tvSeries.cast.length);
+        result.push(tvSeries.cast[randnum]);
+        tvSeries.cast.splice(randnum, 1);
+    }
+    return result;
+}
+
+document.write(randomizeCast(askTvSeries()));
